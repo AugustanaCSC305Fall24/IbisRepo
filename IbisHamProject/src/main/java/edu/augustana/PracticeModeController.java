3f9ac18f5e9e4dc9ddd183abc837ce0fa53dc6d6
ian1111 <iansspamfun11@gmail.com>
2024-10-11 20:26:06 -0500

    added code to practice controller to save the values from the settings page to use when pratice mode is launched. the values do not save until the user clicks launch

--- IbisHamProject/src/main/java/edu/augustana/PracticeModeController.java
+++ IbisHamProject/src/main/java/edu/augustana/PracticeModeController.java
@@ -1,13 +1,31 @@
 package edu.augustana;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Slider;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class PracticeModeController {
+
+    //element for the quality slider
+    @FXML
+    private Slider qualitySlider;
+
+    //element for the amount  slider
+    @FXML
+    private Slider amountSlider;
+
+    //element for the speed slider
+    @FXML
+    private Slider speedSlider;
+
+    //element for the visualizer checkbox
+    @FXML
+    private CheckBox visualizerCheckBox;
 
     //Button action method to switch screens to the morse code dictionary fxml file by the controller file
     @FXML
@@ -36,12 +54,21 @@ public class PracticeModeController {
     //Button action method to switch screens to the launched practice fxml file by the controller file
     @FXML
     private void switchToPracLaunch() throws IOException {
+        //got the values from the sliders and checkbox
+        double quality = qualitySlider.getValue();
+        double amount = amountSlider.getValue();
+        double speed = speedSlider.getValue();
+        boolean isVisualizerEnabled = visualizerCheckBox.isSelected();
+
+        //print values to the command line for testing
+        //System.out.println("Quality Slider Value: " + quality);
+        //System.out.println("Amount Slider Value: " + amount);
+        //System.out.println("Speed Slider Value: " + speed);
+        //System.out.println("Visualizer Enabled: " + isVisualizerEnabled);
+
+        //switch to the launched practice screen
         App.setRoot("pracLaunched");
     }
-
-
-
-
 
 }
 
